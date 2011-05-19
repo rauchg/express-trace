a9e8eb22e861bc9b89206d359a64099c4fb817af
Tj Holowaychuk <tj@vision-media.ca>
2011-05-19 16:22:12 -0700

    example

--- examples/basic.js
+++ examples/basic.js
@@ -10,6 +10,28 @@ var trace = require('../')
 app.use(express.favicon());
 app.use(express.static(__dirname));
 
+function ensureUsername(name) {
+  return function ensureUsername(req, res, next){
+    if (req.params.name == name) {
+      next();
+    } else {
+      next('route');
+    }
+  }
+}
+
+app.get('/user/:name', ensureUsername('tj'), function(req, res, next){
+  res.send('loaded tj');
+});
+
+app.get('/user/:name', function(req, res, next){
+  setTimeout(next, 1000);
+});
+
+app.get('/user/:name', function(req, res, next){
+  res.send('loaded user ' + req.params.name);
+});
+
 app.get('/file/*', function(req, res, next){
   var file = req.params[0];
   res.send('requested ' + file);
